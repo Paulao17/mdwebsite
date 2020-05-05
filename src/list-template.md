1241637288c8098b0346b82ec2d7daaa43eab61a
Paul ALNET <paul@shimaore.net>
2020-05-05 17:54:28 +0200

    Corrections

--- src/list-template.md
+++
@@ -1,3 +0,0 @@
-$if(title)$
-- [$pagetitle$]($sourcefile$)
-$endif$
--- test.sh
+++ test.sh
@@ -21,18 +21,19 @@ for file in src/*.md src/*/*.md
   do
     if [ -f "$file" ]
     then
-      echo " converting $file to pdf"
-      pandoc --template src/pdf-heading-template.html $file -f gfm --metadata-file $(echo $file | sed '$s/\.md$/.yaml/') -o build/temp-header.html # Building pdf header
-      mdpdf $file $(echo $file | sed '$s/\.md$/.pdf/' | sed 's/src/build/') #--header=build/temp-header.html --f-height=0 --hHeight 22
-      rm build/temp-header.html # Remove pdf header after building pdf
       echo " converting $file to html"
       pandoc -s -o $(echo $file | sed '$s/\.md$/.html/' | sed 's/src/build/') -f gfm -c /static/github.css -c /static/style.css --metadata-file $(echo $file | sed '$s/\.md$/.yaml/') $file --template src/template.html
       echo " adding $file to index"
-      pandoc --template src/list-template.md --metadata-file $(echo $file | sed '$s/\.md$/.yaml/') $file 2>&1 | sed 's/src\//build\//g' >> build/index.md # 2>&1 -> redirect stderr to stdout ; sed -> change src/ to build/
+      pandoc --template src/list-template.txt --metadata-file $(echo $file | sed '$s/\.md$/.yaml/') $file 2>&1 | sed 's/src\///g' | sed 's/.md/.html/' | sed 's/.md/.pdf/' >> build/index.md # 2>&1 -> redirect stderr to stdout ; sed -> correct links
       echo " copying $file to build"
       cp $file $(echo $file | sed 's/src/build/')
+      echo " converting $file to pdf"
+      pandoc --template src/pdf-heading-template.html $file -f gfm --metadata-file $(echo $file | sed '$s/\.md$/.yaml/') -o build/temp-header.md # Building pdf header
+      cat $(echo $file | sed 's/src/build/') >> build/temp-header.md
+      mv build/temp-header.md $(echo $file | sed 's/src/build/') # Replace old by md with header
+      mdpdf $(echo $file | sed 's/src/build/')
     fi
 done
 
 echo "Creating index.html"
-pandoc -s -o build/index.html -f gfm -c /static/github.css -c /static/style.css --metadata-file src/index.yaml build/index.md --template template.html
+pandoc -s -o build/index.html -f gfm -c /static/github.css -c /static/style.css --metadata-file src/index.yaml build/index.md --template src/template-index.html
